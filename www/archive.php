3af26619133b961ae0878e40d4bd2492cfcbbda0
Leigh B Stoller <stoller@flux.utah.edu>
2022-01-26 12:23:14 -0700

    Add button to download archive of all data fails in a tar.gz file.

---
+++ www/archive.php
@@ -0,0 +1,34 @@
+<?php
+#
+# Copyright (c) 2000-2022 University of Utah and the Flux Group.
+# 
+# {{{EMULAB-LICENSE
+# 
+# This file is part of the Emulab network testbed software.
+# 
+# This file is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version.
+# 
+# This file is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+# License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this file.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# }}}
+#
+
+$filename = 'monitor-' . date(DATE_ATOM) . ".tar.gz";
+
+header("Content-Type: application/x-tgz");
+header('Content-Disposition: attachment; filename="' . $filename . '"');
+$cmd = "tar -cz *.gz";
+
+passthru($cmd);
+exit(0);
+
+?>
