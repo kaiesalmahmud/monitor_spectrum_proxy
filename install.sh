852a68c4c98ad638aa88f3f5b3dc011c0c308f16
Leigh B Stoller <stoller@flux.utah.edu>
2023-05-22 14:48:34 -0600

    More tweaks

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-sudo apt-get -y install --no-install-recommends python3-rfmonitor rfmonitor-calibration python3-tk python3-matplotlib socat tigervnc-standalone-server autocutsel fvwm
+sudo apt-get -y install --no-install-recommends python3-rfmonitor rfmonitor-calibration python3-tk python3-matplotlib socat tigervnc-standalone-server autocutsel fvwm sendmail-bin
 if [ $? -ne 0 ]; then
     echo 'apt-get install support failed'
     exit 1
