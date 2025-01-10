b58f650886024f99ba34f20004957fa3076c47b6
Leigh B Stoller <stoller@flux.utah.edu>
2025-01-10 16:55:42 -0700

    Bad spelling.

--- profiles/rdz-ondemand.py
+++ profiles/rdz-ondemand.py
@@ -19,7 +19,7 @@ tour.Description(ig.Tour.TEXT, "Allocate all radios and run the monitor");
 #
 # Defaults for this setup
 #
-IMAGE     = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-GRE310"
+IMAGE     = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-GR310"
 MS        = "urn:publicid:IDN+emulab.net+authority+cm"
 COMMAND   = "/local/repository/monitor.pl -K "
 RANGE     = "3350e6-3750e6"
